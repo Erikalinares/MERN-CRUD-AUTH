e2b0c61e479ceeaaa51804a4be0af565e7d3afc7
Erikalinares <erikalinares421@gmail.com>
2023-10-03 11:57:06 +0200

    actualizacion navbar

--- cliente/src/components/Navbar.jsx
+++ cliente/src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
     <nav className='bg-zinc-400 my-0 flex justify-between py-5 px-10 '>
-        <h1>Tasks Manager</h1>
-        <ul>
-            <li></li>
+        <h1 className='text-2xl font-bold '>Tasks Manager</h1>
+        <ul className='flex gap-x-2'>
+            <li>
+               <Link to='/login'>Login</Link>
+            </li>
+            <li>
+               <Link to='/register'>Register</Link>
+            </li>
         </ul>
     </nav>
     
